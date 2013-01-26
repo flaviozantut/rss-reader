6d00ae36e1d1dadfc596558016ef3b8f1b40f8e2
flaviozantut <flaviozantut@gmail.com>
2013-01-26 00:25:26 -0200

    first previw

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,36 @@
 jQuery(document).ready(function($) {
     //alert('teste');
+    var width = $('.rss-news li:first')
+        .addClass('active')
+        .width();
+    $('#rss-prev').addClass('stop');
+    $('.rss-news li').each(function(i, val){
+        $(this).css('left', (i)*width);
+    });
+    $('#rss-next').click(function(){
+        if( !$('.rss-news li:last').hasClass('active') ){
+            $('.rss-news .active').removeClass('active').next().addClass('active');
+            $('.rss-news li').each(function(i, val){
+                $(this).css('left', (parseInt($(this).css('left')))-width);
+            });
+            $(this).removeClass('stop');
+            $('#rss-prev').removeClass('stop');
+        }else{
+            $(this).addClass('stop');
+
+        }
+    });
+
+    $('#rss-prev').click(function(){
+        if( !$('.rss-news li:first').hasClass('active') ){
+            $('.rss-news .active').removeClass('active').prev().addClass('active');
+            $('.rss-news li').each(function(i, val){
+                $(this).css('left', (parseInt($(this).css('left')))+width);
+            });
+            $(this).removeClass('stop');
+            $('#rss-next').removeClass('stop');
+        }else{
+            $(this).addClass('stop');
+        }
+    });
 });
